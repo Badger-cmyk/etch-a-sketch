f05a3d18d2b6a49684f1763db8521f278e50ea3d
Eben Badger <ebenbadger16@gmail.com>
2026-04-22 16:12:05 +0000

    Add function to generate grid dynamically

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const container = document.querySelector('.container')
+
+
+function createGrid(n){
+    const size = 640/n
+
+    for(let row = 0; row < n; row++){
+        for(let column = 0; column < n; column++){
+            const cell = document.createElement('div')
+            cell.classList.add('cell')
+            cell.style.width = `${size}px`
+            cell.style.height = `${size}px`
+            container.append(cell)
+
+        }
+    }
+}
